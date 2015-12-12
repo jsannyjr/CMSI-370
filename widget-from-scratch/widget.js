0c0bc8b481ca3186dd33fc5fed96b4ef05934c37
jsannyjr <jsanny1@lion.lmu.edu>
2015-12-11 17:55:59 -0800

    Cleaning up the code

--- widget-from-scratch/widget.js
+++ widget-from-scratch/widget.js
@@ -1,15 +1,23 @@
 (function($){
 	var dragging;
+	var handler = function(event){
+  			dragging.offset({left:event.pageX, top:event.pageY});
+  		};
+  	var cleanUp = function(event){
+  			dragging.remove();
+  			$("body").unbind("mousemove", handler);
+  		}
   $.fn.widget= function(){
   	this.mousedown(function (event){
-  		dragging = $(event.target).clone();
-  		$("body").append(dragging);
-  		$("body").mousemove(function(event){
-  			dragging.offset({left:event.pageX, top:event.pageY});
-  		});
-  		dragging.mouseup(function(event){
-  			dragging.remove();
-  		});
+  		dragging = $(event.target).clone()
+  			.addClass("dragging-image")
+  			.offset({left: event.pageX, top:event.pageY});
+
+  		$("body")
+  			.append(dragging)
+  			.mousemove(handler);
+
+  		dragging.mouseup(cleanUp);
   	});
   };
   $(".example").widget();
